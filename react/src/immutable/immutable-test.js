21eaf2f18123366d0c7ea07454e789c4bcc98d6c
Rick <cyanyao@live.com>
2018-04-03 21:54:40 +0800

    调整

---
+++ react/src/immutable/immutable-test.js
@@ -0,0 +1,9 @@
+// @flow
+
+import React from 'react';
+import { List, Map } from 'immutable';
+
+export default class immutableTest extends React.Component<{}> {
+
+}
+
--- react/src/index.js
+++ react/src/index.js
@@ -3,9 +3,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Test from './transition-test';
-import Db from './dbtester';
+
+import Test from './animation/transition-test';
+import Db from './db/dbtester';
 import registerServiceWorker from './registerServiceWorker';
 
 const root = document.getElementById('root');
@@ -16,7 +16,6 @@ if (!root) {
 
 ReactDOM.render(
   <div>
-    {/* <App /> */}
     {/* <Test.Demo2 /> */}
     <Test.Demo1 />
     <Db />
