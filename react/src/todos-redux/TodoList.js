f2e948704339205660a1545388d5ebd84db82386
Rick <cyanyao@live.com>
2018-05-09 00:15:46 +0800

    initial todolist demo

---
+++ react/src/todos-redux/TodoList.js
@@ -0,0 +1,18 @@
+// @flow
+
+import React from 'react';
+
+import './todolist.css';
+
+type Props = {
+
+}
+
+export default class TodoList extends React.Component<Props> {
+
+  render() {
+    return (
+      <div className="todo-list">Editing</div>
+    )
+  }
+}
