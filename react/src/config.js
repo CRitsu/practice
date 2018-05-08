5009f1eaa6524f1aead3543f4476a612f048ffe9
Rick <cyanyao@live.com>
2018-05-08 23:48:13 +0800

    update the catalog

--- react/src/config.js
+++ react/src/config.js
@@ -4,12 +4,12 @@ export default {
   title: 'DEMO 传送门',
   links: [
     {
-      label: 'Calculator Redux demo (Working)',
-      link: '/calculator-redux',
-    },
-    {
       label: 'Todo list Redux demo (Working)',
       link: '/todo-list-redux',
+    },
+    {
+      label: 'Calculator Redux demo',
+      link: '/calculator-redux',
     },
     {
       label: 'DB tester NeDB.js',
